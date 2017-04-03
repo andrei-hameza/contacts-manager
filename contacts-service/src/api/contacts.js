19d966cff508164a0bcddd6fafdff02e12be00d2
Andrei <Andrei_Hameza@epam.com>
2017-04-03 23:52:54 +0300

    Adds controller for communicating with Contacts model

--- contacts-service/src/api/contacts.js
+++ contacts-service/src/api/contacts.js
@@ -1,60 +1,16 @@
 'use strict'
 
-const status = require('http-status')
 const contactsRouter = require('express').Router()
-const config = require('../config/config.js')
-const _ = require('lodash')
-const OFFSET = config.get('default:api:offset')
-const LIMIT = config.get('default:api:limit')
+const contactController = require('../controllers/contact')
 
-const contacts = []
-var id = 0
+contactsRouter.get('', contactController.getContactsList)
 
-contactsRouter.get('', (req, res) => {
-  const { offset = OFFSET, limit = LIMIT } = req.query
-  const data = contacts.slice(parseInt(offset), parseInt(offset) + parseInt(limit))
-  res.status(status.OK).json(data)
-})
+contactsRouter.get('/:id', contactController.getContact)
 
-contactsRouter.get('/:id', (req, res) => {
-  const { id } = req.params
-  const contact = _.find(contacts, { id: id })
-  if (!contact) {
-    res.status(status.NOT_FOUND).send()
-    return
-  }
-  res.status(status.OK).json(contact)
-})
+contactsRouter.post('/', contactController.createContact)
 
-contactsRouter.post('/', (req, res) => {
-  const contact = req.body
-  const contactId = id++
-  const newContact = Object.assign({ id: contactId.toString() }, contact)
-  contacts.push(newContact)
-  res.status(status.OK).json(newContact)
-})
+contactsRouter.delete('/:id', contactController.deleteContact)
 
-contactsRouter.patch('/:id', (req, res) => {
-  const { id } = req.params
-  const contact = _.find(contacts, { id: id })
-  if (!contact) {
-    res.status(status.NOT_FOUND).send()
-    return
-  }
-  Object.assign(contact, req.body)
-  res.status(status.OK).json(contact)
-})
-
-contactsRouter.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const contactIndex = _.findIndex(contacts, { id: id })
-
-  if (contactIndex < 0) {
-    res.status(status.NOT_FOUND).send()
-    return
-  }
-  const [deletedContact] = contacts.splice(contactIndex, 1)
-  res.status(status.OK).json(deletedContact)
-})
+contactsRouter.patch('/:id', contactController.updateContact)
 
 module.exports = contactsRouter
