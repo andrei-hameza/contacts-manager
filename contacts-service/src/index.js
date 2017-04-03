6e01c5cedee61e1c43f77af847c908185a27e767
Andrei <Andrei_Hameza@epam.com>
2017-04-03 15:01:09 +0300

    Code refactoring: async\await is now used to start app

--- contacts-service/src/index.js
+++ contacts-service/src/index.js
@@ -1,14 +1,18 @@
+/* eslint-env node */
 const server = require('./server/server.js')
-const options = { port: 3000 }
+const options = { port: null }
 
 console.log('### Contacts service ###')
 
 console.log('Starting http server...')
 
-server
-  .start(options)
-  .then((port) => {
+async function start () {
+  try {
+    await server.start(options)
     console.log('Server started on port: ', options.port)
-  }).catch(() => {
+  } catch (e) {
     console.error('Server couldn\'t start. Please, define a port.')
-  })
+  }
+}
+
+start()
