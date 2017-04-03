09b121aa8dd0e68945b106422f862ee5ce390c3a
Andrei <Andrei_Hameza@epam.com>
2017-04-03 17:48:56 +0300

    Adds routes for CRUD operations

---
+++ contacts-service/src/api/contacts.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const status = require('http-status')
+const contactsRouter = require('express').Router()
+const apiDefault = require('../config/config.js').get('default')
+const _ = require('lodash')
+const OFFSET = _.get(apiDefault, ['api', 'offset'])
+const LIMIT = _.get(apiDefault, ['api', 'limit'])
+
+const contacts = []
+var id = 0
+
+contactsRouter.get('', (req, res) => {
+  const { offset = OFFSET, limit = LIMIT } = req.query
+  const data = contacts.slice(parseInt(offset), parseInt(offset) + parseInt(limit))
+  res.status(status.OK).json(data)
+})
+
+contactsRouter.get('/:id', (req, res) => {
+  const { id } = req.params
+  const contact = _.find(contacts, { id: id })
+  if (!contact) {
+    res.status(status.NOT_FOUND).send()
+    return
+  }
+  res.status(status.OK).json(contact)
+})
+
+contactsRouter.post('/', (req, res) => {
+  const contact = req.body
+  const contactId = id++
+  const newContact = Object.assign({ id: contactId.toString() }, contact)
+  contacts.push(newContact)
+  res.status(status.OK).json(newContact)
+})
+
+contactsRouter.patch('/:id', (req, res) => {
+  const { id } = req.params
+  const contact = _.find(contacts, { id: id })
+  if (!contact) {
+    res.status(status.NOT_FOUND).send()
+    return
+  }
+  Object.assign(contact, req.body)
+  res.status(status.OK).json(contact)
+})
+
+contactsRouter.delete('/:id', (req, res) => {
+  const { id } = req.params
+  const contactIndex = _.findIndex(contacts, { id: id })
+
+  if (contactIndex < 0) {
+    res.status(status.NOT_FOUND).send()
+    return
+  }
+  const [deletedContact] = contacts.splice(contactIndex, 1)
+  res.status(status.OK).json(deletedContact)
+})
+
+module.exports = contactsRouter
