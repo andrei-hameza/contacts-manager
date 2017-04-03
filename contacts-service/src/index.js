e6528f6cfd262d8813ad13da1a21059df6c9b6ab
Andrei <Andrei_Hameza@epam.com>
2017-04-03 20:47:17 +0300

    Adds mogodb connection via mongoose

--- contacts-service/src/index.js
+++ contacts-service/src/index.js
@@ -1,19 +1,26 @@
 'use strict'
 
 const server = require('./server/server.js')
+const db = require('./db')
 const config = require('./config/config')
 const port = config.get('port')
+const uri = config.get('mongoose:uri')
 
 console.log('### Contacts service ###')
 
 console.log('Starting http server...')
 
 async function start () {
+  let app
+
   try {
-    await server.start({ port })
+    await db.connect({ uri })
+    app = await server.start({ port })
+    app.on('close', () => db.disconnect())
     console.log('Server started on port: ', port)
   } catch (e) {
-    console.error('Server couldn\'t start. Please, define a port.')
+    console.error('Server couldn\'t start.')
+    console.error(e.message)
   }
 }
 
