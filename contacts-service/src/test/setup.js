b62d715cbb28d3b33c4a97b7062369ee1e00ef9a
Andrei <Andrei_Hameza@epam.com>
2017-04-13 17:42:47 +0300

    Removes unused dependecies

--- contacts-service/src/test/setup.js
+++ contacts-service/src/test/setup.js
@@ -1,14 +1,8 @@
 'use strict'
 
 const sinon = require('sinon')
-const chai = require('chai')
-const sinonChai = require('sinon-chai')
 
 process.env.PORT = 3000
-
-before(() => {
-  chai.use(sinonChai)
-})
 
 beforeEach(function beforeEach () {
   this.sandbox = sinon.sandbox.create()
